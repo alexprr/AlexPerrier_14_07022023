a20160e6f76fac791e39a30abdec0fa60aa53be0
Alex <alexperrier.pro@gmail.com>
2023-02-08 12:21:04 +0100

    add Modal component

---
+++ src/components/CreateEmployee/Modal.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Modal = ({ open, onClose }) => {
+    if(!open) return null
+
+  return (
+    <div className='modal'>
+        <div className="modal__container">
+            <div className="modal__content">
+                <p className="modal__text">New Employee created !</p>
+            </div>
+            <div>
+                <i onClick={onClose} className="uil uil-multiply modal__icon"></i>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Modal
