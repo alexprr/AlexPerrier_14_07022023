e46241c8438040d3c3771c93d6f5dfc6be89e154
Alex <alexperrier.pro@gmail.com>
2023-02-08 10:15:04 +0100

    add employee list component to project

---
+++ src/components/EmployeeList/EmployeeList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Header } from "../index"
+import "./employee-list.css"
+
+const EmployeeList = () => {
+  return (
+    <div>
+        <Header />
+    </div>
+  )
+}
+
+export default EmployeeList
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
-export { default as CreateEmployee } from "../components/CreateEmployee/CreateEmployee";
 export { default as Header } from "../components/Header/Header";
+export { default as CreateEmployee } from "../components/CreateEmployee/CreateEmployee";
+export { default as EmployeeList } from "../components/EmployeeList/EmployeeList";
