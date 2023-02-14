385df6106c163232120b147502dcaed4b4616ad9
Alex <alexperrier.pro@gmail.com>
2023-02-14 10:55:39 +0100

    feat: add new version of EmployeeList page

--- src/components/EmployeeList/EmployeeList.jsx
+++ src/components/EmployeeList/EmployeeList.jsx
@@ -6,6 +6,20 @@ const EmployeeList = () => {
   return (
     <div>
         <Header />
+
+        <main className='main employees__container'>
+            <section className="employees flex">
+                <div className="employees__content">
+                    <div className='employees__title'>
+                        <h2>
+                            Current Employees
+                            <i className="uil uil-user employees__title-icon"></i>
+                        </h2>
+                        <a href="/" className='employees__title-link'>Home</a>
+                    </div>
+                </div>
+            </section>
+        </main>
     </div>
   )
 }
