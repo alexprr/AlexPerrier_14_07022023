4616ba28056fb926e94d2f959d20374ad7302f3e
Alex <alexperrier.pro@gmail.com>
2023-02-15 15:00:09 +0100

    create export for table component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from "../components/Header/Header";
 export { default as CreateEmployee } from "../components/CreateEmployee/CreateEmployee";
 export { default as EmployeeList } from "../components/EmployeeList/EmployeeList";
+export { default as EmployeeTable } from "../components/EmployeeTable/EmployeeTable";
