eefd197044c94cf7fe1779a9ad8f47a925ca00ab
Alex <alexperrier.pro@gmail.com>
2023-02-20 15:02:22 +0100

    add project logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import "./header.css"
 
+import Logo from "../../assets/logo.jpg"
+
 const Header = () => {
   return (
     <header className='header flex'>
+        <img src={Logo} alt="Logo" />
         <h1 className="header__title">HRnet</h1>
     </header>
   )
