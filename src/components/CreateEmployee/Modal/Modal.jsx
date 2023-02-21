94ef9f484483753d3bd6d6c4d99f2e8498d32f7f
Alex <alexperrier.pro@gmail.com>
2023-02-21 12:50:02 +0100

    rework style and behaviour of modal component

--- src/components/CreateEmployee/Modal/Modal.jsx
+++ src/components/CreateEmployee/Modal/Modal.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 
+import { Link } from "react-router-dom";
+
+import "./modal.css"
+
 const Modal = ({ onClose }) => {
     return (
-        <div className='modal'>
+        <div className='modal' onClick={onClose}>
             <div className="modal__container">
                 <div className="modal__content">
-                    <p className="modal__text">New Employee created !</p>
+                    <div className='modal__title'>
+                        <i class="fa-solid fa-user-plus modal__content-icon"></i>
+                        <p className="modal__text">New Employee Created !</p>
+                    </div>
+                    <div className="modal__description">
+                        <p>Click on the button to check employees list.</p>
+                    </div>
                 </div>
-                <div>
-                    <i onClick={onClose} className="uil uil-multiply modal__icon"></i>
+                
+                <Link to="employees-list" className='button modal__button'>
+                    Go to list
+                </Link>
+                
+                <div className='modal__close'>
+                    <i onClick={onClose} class="fa-solid fa-xmark modal__close-icon"></i>
                 </div>
             </div>
         </div>
