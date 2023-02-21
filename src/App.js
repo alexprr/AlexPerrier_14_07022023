971df08766f5fd0f9dd657699714c6a7a0855ec5
Alex <alexperrier.pro@gmail.com>
2023-02-21 21:23:47 +0100

    add dynamic path to employees list

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { CreateEmployee, EmployeeList } from "./components/index";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<CreateEmployee />} />
-        <Route path="/employees-list" element={<EmployeeList />} />
-      </Routes>
-    </div>
-  );
+	return (
+		<div>
+			<Routes>
+				<Route path="/" element={<CreateEmployee />} />
+				<Route path=":employee" element={<EmployeeList />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
